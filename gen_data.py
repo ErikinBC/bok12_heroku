e1c52b6f7977dbc46e2155f0bcfdc7f4a7e53904
ErikinBC <alla_hack@hotmail.com>
2022-01-06 19:08:52 -0500

    removing dict subfolder

--- gen_data.py
+++ gen_data.py
@@ -25,40 +25,15 @@ import pandas as pd
 from time import time
 from funs_cipher import encipherer
 
-########################
-# --- (1) GET DATA --- #
+##########################
+# --- (1) DICTIONARY --- #
 
-# (i) n-gram data from
-url_1gram = 'http://norvig.com/ngrams/count_1w.txt'
-fn_1gram = 'words_ngram.txt'
+# source: http://wordlist.aspell.net/12dicts/
 
-if not os.path.exists(fn_1gram):
-    os.system('wget -q -O %s %s' % (fn_1gram, url_1gram))
+url_dict='https://cfhcable.dl.sourceforge.net/project/wordlist/12Dicts/6.0/12dicts-6.0.2.zip'
 
-df_1gram = pd.read_csv(fn_1gram, sep='\t',header=None,na_values='',keep_default_na=False)
-df_1gram.rename(columns={0:'word', 1:'n'}, inplace=True)
 
-# (ii) simple english words
-url_english = 'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt'
-fn_english = 'words_english.txt'
 
-if not os.path.exists(fn_english):
-    os.system('wget -q -O %s %s' % (fn_english, url_english))
-
-df_english = pd.read_csv(fn_english, sep='\t',header=None,na_values='',keep_default_na=False)
-df_english.rename(columns={0:'word'}, inplace=True)
-
-# (iii) Keep only intersection
-df_merge = df_1gram.merge(df_english,'inner')
-# Check that it has same words as xenotext
-xenotext = pd.read_csv('xenotext.txt',header=None)[0].to_list()
-xenotext = sum([z.split(' ') for z in xenotext], [])
-xenowords = np.unique(xenotext)
-word_match = df_merge['word'][df_merge['word'].isin(xenowords)]
-word_diff = np.setdiff1d(xenowords, word_match)
-assert len(word_diff) == 0, 'Missing words: %s' % word_diff
-
-# (iv) 12 dicts data: http://wordlist.aspell.net/12dicts/
 annos = '%:&#=<^~+$'
 annos = '|'.join(['\\'+anno for anno in list(annos)])
 
@@ -111,6 +86,7 @@ for i, word in enumerate(df_12['word']):
         holder.append(res)
 res_dict = pd.concat(holder)
 
+
 ##########################
 # --- (2) ENCIPHERED --- #
 
@@ -121,4 +97,38 @@ self.score_ciphers(cn_weight='n')
 self.df_score
 self.df_encipher
 
+
+
+########################
+# --- (X)  --- #
+
+# # (i) n-gram data from
+# url_1gram = 'http://norvig.com/ngrams/count_1w.txt'
+# fn_1gram = 'words_ngram.txt'
+
+# if not os.path.exists(fn_1gram):
+#     os.system('wget -q -O %s %s' % (fn_1gram, url_1gram))
+
+# df_1gram = pd.read_csv(fn_1gram, sep='\t',header=None,na_values='',keep_default_na=False)
+# df_1gram.rename(columns={0:'word', 1:'n'}, inplace=True)
+
+# # (ii) simple english words
+# url_english = 'https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt'
+# fn_english = 'words_english.txt'
+
+# if not os.path.exists(fn_english):
+#     os.system('wget -q -O %s %s' % (fn_english, url_english))
+
+# df_english = pd.read_csv(fn_english, sep='\t',header=None,na_values='',keep_default_na=False)
+# df_english.rename(columns={0:'word'}, inplace=True)
+
+# # (iii) Keep only intersection
+# df_merge = df_1gram.merge(df_english,'inner')
+# # Check that it has same words as xenotext
+# xenotext = pd.read_csv('xenotext.txt',header=None)[0].to_list()
+# xenotext = sum([z.split(' ') for z in xenotext], [])
+# xenowords = np.unique(xenotext)
+# word_match = df_merge['word'][df_merge['word'].isin(xenowords)]
+# word_diff = np.setdiff1d(xenowords, word_match)
+# assert len(word_diff) == 0, 'Missing words: %s' % word_diff
 
